0a47cf93d9b906ad67e13dc1916c99cd21207dc0
NaveenChahar <40751814+NaveenChahar@users.noreply.github.com>
2020-12-07 10:44:51 +0530

    Add files via upload

---
+++ precache-manifest.ff051d50e2a14b7bebd487b53bcbcdaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b7388da3bd68c061c0087a2048bbac",
+    "url": "/Job-Assignment/index.html"
+  },
+  {
+    "revision": "65a6b5d60a1c94c2c314",
+    "url": "/Job-Assignment/static/css/main.521e6932.chunk.css"
+  },
+  {
+    "revision": "bf39f471e8d4e0f4eaec",
+    "url": "/Job-Assignment/static/js/2.66a9379c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Job-Assignment/static/js/2.66a9379c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65a6b5d60a1c94c2c314",
+    "url": "/Job-Assignment/static/js/main.eae6bb33.chunk.js"
+  },
+  {
+    "revision": "a14570ad9a40e38c7ff5",
+    "url": "/Job-Assignment/static/js/runtime-main.854c23d6.js"
+  }
+]);
